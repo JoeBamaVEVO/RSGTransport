dc26ab43222df46b19e2c16fc57c62ca44acdb39
Kristian Grøsvik <kristian.grosvik04@gmail.com>
2023-08-19 02:05:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include 'head.php'?>
+</head>
+<body>
+   <?php include 'navbar.php'?>
+   <div class="container-fluid">
+        <div class="container">
+            <div class="container-flex">
+                <div class="container-text">
+                    <p class="PageHeader">Trenger du frakt, graving, <br>eller planering?</p>
+                    <p class="PageText">Rolfen hjelper deg.<br>  
+                        Uansett om du trenger varetransport eller maskinarbeid: <br>
+                        Rolfen kan hjelpe deg med alt av transportjenester, <br>
+                        og andre små oppdrag. <br>
+                    </p>
+                    <p class="PageInfo">Les mer om mine Tjenester her</p>
+                    <a href="tjenester.php">
+                        <button class="TjenesterBtn">
+                            Mine Tjenester
+                        </button> 
+                    </a>
+                </div>
+                <img src="/bilder/Ihi28Gravemaskin.jpg" alt="">
+            </div>
+           
+           
+        </div>
+   </div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,21 @@
+<?php include 'head.php'?>
+<nav class="Navbar">
+  <div class="NavItemWrapper">
+    <div class="" id="navbarSupportedContent">
+      <ul class="">
+        <li class="NavLogo">
+          <a href="#">RSGTransport</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Hjem</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#">Tjenester</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kontakt meg</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
