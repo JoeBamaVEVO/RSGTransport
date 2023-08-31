f7057adec1464034ba0c0f640a6a365c38770897
JoeBamaVEVO <Kristian.grosvik04@gmail.com>
2023-08-31 20:04:06 +0200

    restructured the html and fixed the footer

--- kontakt.php
+++ kontakt.php
@@ -12,43 +12,37 @@
 </head>
 <body>
 <body>
-<main>
-    <content>
-        <div class="container-fluid">
-            <div class="container">
-                <div class="container-flex">
-                    <div class="container-text">
-                        <p class="PageHeader">Ta kontakt</p>
-                        <p class="PageText">  
-                            Jeg ønsker å gjøre det så enkelt som mulig for deg å komme i kontakt med oss. <br>
-                            Vennligst bruk følgende kontaktinformasjon for å nå meg. <br>
-                            Jeg ser frem til å høre fra deg!
-                        </p>
-                        <p class="PageHeader2">
-                            Epost:
-                        </p>
-                        <p class="PageText">
-                            kontakt@rolfen.no
-                        </p>
-                        <p class="PageHeader2">
-                            Telefon:
-                        </p>
-                        <p class="PageText">
-                            123 45 678
-                        </p>
-                        <p class="PageInfo">Send oss en e-post her</p>
-                        <a href="mailto:kontakt@rolfen.no">
-                            <button class="TjenesterBtn">
-                                Send en e-post
-                            </button> 
-                        </a>
-                    </div>
-                    <!-- <img src="/bilder/Ihi28Gravemaskin.jpg" alt=""> -->
-                </div>
-            </div>
+<content>
+    <div class="container-flex">
+        <div class="container-text">
+            <p class="PageHeader">Ta kontakt</p>
+            <p class="PageText">  
+                Jeg ønsker å gjøre det så enkelt som mulig for deg å komme i kontakt med oss. <br>
+                Vennligst bruk følgende kontaktinformasjon for å nå meg. <br>
+                Jeg ser frem til å høre fra deg!
+            </p>
+            <p class="PageHeader2">
+                Epost:
+            </p>
+            <p class="PageText">
+                kontakt@rolfen.no
+            </p>
+            <p class="PageHeader2">
+                Telefon:
+            </p>
+            <p class="PageText">
+                123 45 678
+            </p>
+            <p class="PageInfo">Send oss en e-post her</p>
+            <a href="mailto:kontakt@rolfen.no">
+                <button class="TjenesterBtn">
+                    Send en e-post
+                </button> 
+            </a>
         </div>
-    </content>
-</main>
+        <!-- <img src="/bilder/Ihi28Gravemaskin.jpg" alt=""> -->
+    </div>
+</content>
 <?php include 'footer.php' ?>
 </body>
 </html>
